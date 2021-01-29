f8aab072c5cdd7e589fa33b6e66642462061397d
Sindre <Sindrees@hotmail.no>
2021-01-29 15:42:12 +0100

    update

--- src/components/BrukerProfil.js
+++ src/components/BrukerProfil.js
@@ -1,6 +1,6 @@
-import React, {Component, useState} from 'react';
+import React, { Component } from 'react';
 import * as Functions from './Functions'
-
+import {storage} from '../server/firebase';
 
 
 
@@ -8,9 +8,23 @@ export function BrukerProfil() {
     class ImageUpload extends Component {
         constructor(props) {
             super(props);
-            this.state = { }
+            this.state = {
+                image: null,
+                url: null
+            }
+            this.handleChange = this.handleChange.bind(this);
+            this.handleUpload = this.handleUpload.bin(this);
         }
     }
+    const handleChange = e => {
+        if(e.target.files[0]){
+            const image = e.target.files[0];
+            this.setState(() => ({image}));
+        }
+    }
+   
+
+
     return (
     <div className="App">
         <div className="row">
@@ -18,9 +32,9 @@ export function BrukerProfil() {
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js" />
                 <form runat="server">
                     <p>Last opp/endre profilbilde: </p>
-                <input type='file' id="imgInp" onChange="readURL(this)" />
+                <input type='file' id="imgInp" onChange="readURL(this)"/>
                 </form>
-                <button className="btn waves-effect waves-light right">Last opp bilde</button>
+                <button className="btn waves-effect waves-light right" >Last opp bilde</button>
             </div>
         </div>
     </div>
