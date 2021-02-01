9fb9519349961b8c703ee40382aa241dc2e6dabc
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-02-01 12:44:42 +0100

    Viser Fornavn og Etternavn på Home siden

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../context/authContext'
+import  { db } from "../server/firebase"
 
 
 export function Home(){
     const { gjeldeneBruker } = useAuth();
+    const [fornavn, setFornavn] = useState()
+    const [etternavn, setEtternavn] = useState()
+
+    db.collection("BrukerInfo").doc(gjeldeneBruker.uid).onSnapshot(function (doc){
+       const firstName = doc.data().Fornavn;
+       const lastName = doc.data().Etternavn;
+       setFornavn(firstName);
+       setEtternavn(lastName);
+       console.log(firstName, lastName)
+    })
 
 
 
@@ -12,6 +23,9 @@ export function Home(){
         <div className="row">
             <div className="col s12 offset-m4 m4 card-panel">
                 <p>Du er logget inn som: </p>
+                <strong>Fornavn: </strong> {fornavn}
+                <strong>Etternavn: </strong> {etternavn}
+                <br/>
                 <strong>Email: </strong> {gjeldeneBruker.email}
                 <br/>
                 <strong>UID: </strong>{gjeldeneBruker.uid}    
