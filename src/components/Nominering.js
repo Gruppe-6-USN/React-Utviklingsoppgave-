b0d63b58d783170c3ce525792c8b9e27be6446f6
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-02-18 13:55:45 +0100

    Update Nominering.js

--- src/components/Nominering.js
+++ src/components/Nominering.js
@@ -1,14 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import  { db } from "../server/firebase"
+import { useAuth } from "../context/authContext";
 
 
 
 export default function Nominering(){
     const [brukere, setBruker] = useState("")
-    
+    const { nominerBruker, gjeldeneBruker } = useAuth()
+    const fornavnRef = useRef()
+    const etternavnRef = useRef()
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        await nominerBruker(fornavnRef.current.value, etternavnRef.current.value)
+        console.log(fornavnRef, etternavnRef)
+    }
     
     useEffect(() => {
         db.collection('BrukerInfo')
+        .where("Nominerbar", "==", true)
         .get()
         .then(snapshot => {
           const documents = snapshot.docs.map(doc => doc.data())
@@ -17,25 +27,21 @@ export default function Nominering(){
         })
     }, [])
 
-
-    
-  
-
     return ( 
-        <div className="App">
+    <div className="App">
         <div className="row">
-
-
-                        { brukere && brukere.map(bruker => { 
-                            return(
-                                <div className="col  m6 card-panel ">
-                                <p>{ bruker.Fornavn } { bruker.Etternavn }</p>
-                                <button type="submit" className="btn waves-effect waves-light .center-block">Nominér</button>
-                                </div> 
-                                )}
-                        )
-                        } 
-
+        { brukere && brukere.map(bruker => { 
+        return(
+            <div className="col  m6 card-panel ">
+                <form onSubmit={ handleSubmit }>
+                    <p ref={ fornavnRef }>{ bruker.Fornavn }</p> 
+                    <p ref={ etternavnRef }>{ bruker.Etternavn }</p>
+                    <button type="submit" className="btn waves-effect waves-light .center-block">Nominér</button>
+                </form>
+            </div> 
+        )}
+        )
+        } 
         </div>
     </div>
      );
