6209409f0d1ce6a1b0283b248d6542ab158b8a40
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-03-19 10:32:02 +0100

    Avstemside

--- src/components/Avstemming.js
+++ src/components/Avstemming.js
@@ -1,7 +1,11 @@
 const Avstemming = () => {
     return ( 
-        <div className="App">
-      
+    <div className="App">
+            <div className="col  m6 card-panel"  >
+                <p ref={ fornavnRef } >  </p>
+                <p ref={ etternavnRef } >  </p>
+                <button  >Stem</button> 
+            </div>
     </div>
      );
 }
