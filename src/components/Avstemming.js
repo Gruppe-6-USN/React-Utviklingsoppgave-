74296e88e61a3a456a178c4d43b51bc55d50733e
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-03-19 12:52:44 +0100

    Avstemm increment

--- src/components/Avstemming.js
+++ src/components/Avstemming.js
@@ -30,19 +30,12 @@ export default function Avstemming() {
         <div className="row">
         <h3>Stem på en kandidat</h3>
         <p>Trykk på "stem" for å stemme på en kandidat. Kandidaten vil da få en stemme</p>
-<<<<<<< Updated upstream
-            <div className="col width-margin m6 card-panel nominerKort"  >
-                <p  >  </p>
-                <p  >  </p>
-                <button  className="float-right btn waves-effect waves-light">Stem</button> 
-=======
         { brukere && brukere.map(bruker => {
         return(
             <div className="col  m6 card-panel" key= {bruker.id} >
                 <p ref={ fornavnRef } > { bruker.Fornavn } </p>
                 <p ref={ etternavnRef } > { bruker.Etternavn } </p>
                 <button onClick= { () => handleSubmit(bruker.id) } >Stem</button> 
->>>>>>> Stashed changes
             </div>
         )}
         )
