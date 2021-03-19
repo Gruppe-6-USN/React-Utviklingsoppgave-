f76164c4e922cab5c7aae5472f06555d066832f5
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-03-19 10:40:43 +0100

    s

--- src/components/Avstemming.js
+++ src/components/Avstemming.js
@@ -1,11 +1,15 @@
 const Avstemming = () => {
     return ( 
     <div className="App">
+        <div className="row">
+        <h3>Stem på en kandidat</h3>
+        <p>Trykk på "stem" for å stemme på en kandidat. Kandidaten vil da få en stemme</p>
             <div className="col  m6 card-panel"  >
-                <p ref={ fornavnRef } >  </p>
-                <p ref={ etternavnRef } >  </p>
+                <p  >  </p>
+                <p  >  </p>
                 <button  >Stem</button> 
             </div>
+        </div>
     </div>
      );
 }
