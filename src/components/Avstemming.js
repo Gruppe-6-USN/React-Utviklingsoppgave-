99ee56b4f4017e5ead99c3887c631c29c292ad3f
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-03-25 12:45:01 +0100

    Update Avstemming.js

--- src/components/Avstemming.js
+++ src/components/Avstemming.js
@@ -5,6 +5,7 @@ import { useAuth } from "../context/authContext";
 export default function Avstemming() {
 
     const [brukere, setBruker] = useState("")
+    const [harStemt, setharStemt] = useState();
     const { stemBruker, brukerHarStemt, gjeldeneBruker } = useAuth()
     const fornavnRef = useRef()
     const etternavnRef = useRef()
@@ -20,11 +21,28 @@ export default function Avstemming() {
         })
     }, [])
 
+    useEffect(() => {
+        const unsub = db.collection('BrukerInfo').doc(gjeldeneBruker.uid)
+        .onSnapshot(function (doc){
+            const stemmeSjekk = doc.data().harStemt;
+            //console.log(stemmeSjekk)
+            if(stemmeSjekk == true){
+                setharStemt(stemmeSjekk)
+            }
+            
+        })
+        
+        return unsub
+    }, [])
+
+    
+
     async function handleSubmit(id) {
         const brukerFinner = brukere.find(bruker => bruker.id === id)
         await stemBruker(brukerFinner.id);
         //console.log(gjeldeneBruker)
         await brukerHarStemt(gjeldeneBruker.uid)
+        
     }
 
     return ( 
@@ -37,7 +55,8 @@ export default function Avstemming() {
             <div className="col width-margin m6 card-panel nominerKort" key= {bruker.id} >
                 <p ref={ fornavnRef } > { bruker.Fornavn } </p>
                 <p ref={ etternavnRef } > { bruker.Etternavn } </p>
-                <button className="float-right btn waves-effect waves-light" onClick= { () => handleSubmit(bruker.id) } >Stem</button> 
+                { !harStemt && <button className="float-right btn waves-effect waves-light" onClick= { () => handleSubmit(bruker.id) } >Stem</button>}
+                { harStemt && <button className="float-right btn waves-effect waves-light" onClick= { () => handleSubmit(bruker.id) } disabled >Stem</button>}
             </div>
         )}
         )
