c2459762faffe5c4bfc8129be6a79c8c634e953d
KristerLassen <70012970+KristerLassen@users.noreply.github.com>
2021-04-09 15:09:23 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/Gruppe-6-USN/React-Utviklingsoppgave- into main""
    This reverts commit fc3c3714c870891744b861d16712bb827dc5d28a.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,23 +22,48 @@ const Navbar = () => {
         }catch {
             setError("Utlogging mislykkes")
         }
-    }
-    return ( 
-    <div className="App">
+    } if(!gjeldeneBruker){
+        return (<div className="App">
       <nav className="deep-purple darken-4">  
       <div className="nav-wrapper container nav">
-    
+        
       <a id="logo-container" href="/" className="brand-logo">USN-valget</a>
       <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <div className="">
               <ul className="right hide-on-med-and-down">
-                  {gjeldeneBruker && <li><NavLink to="/">Hjem</NavLink></li>}
-                  {gjeldeneBruker && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
-                  {gjeldeneBruker && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
-                  {gjeldeneBruker && <li><NavLink to="/BrukerProfil">Profil</NavLink></li>}
+        {<li><NavLink to="/Logginn">Logg inn</NavLink></li>}
+        {<li><NavLink to="/Registrering">Registrering</NavLink></li>}
+        {error && <p>{error}</p>}
+        </ul>
+        <ul id="nav-mobile" className="sidenav">
+        {<li><NavLink to="/Logginn">Logg inn</NavLink></li>}
+        {<li><NavLink to="/Registrering">Registrering</NavLink></li>}
+        {<button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
+        </ul>
+        </div>
+        </div>
+        </nav>
+        </div> 
+        
+        )
+
+    } else {
+    return ( 
+    <div className="App">
+      <nav className="deep-purple darken-4">  
+      <div className="nav-wrapper container nav">
+        
+      <a id="logo-container" href="/" className="brand-logo">USN-valget</a>
+      <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <div className="">
+              <ul className="right hide-on-med-and-down">
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/">Hjem</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/BrukerProfil">Profil</NavLink></li>}
                   {!gjeldeneBruker && <li><NavLink to="/Logginn">Logg inn</NavLink></li>}
                   {!gjeldeneBruker && <li><NavLink to="/Registrering">Registrering</NavLink></li>}
-                  {gjeldeneBruker && <button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
+                  {<button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
                   {error && <p>{error}</p>}
               </ul>
               
@@ -62,6 +87,7 @@ const Navbar = () => {
       
     
      );
+}
 }
  
 export default withRouter(Navbar);
