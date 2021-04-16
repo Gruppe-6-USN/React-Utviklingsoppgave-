fb174742081f8a045691eabe1555a3c9599a6f72
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-04-16 09:39:57 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,19 @@
 import { NavLink, withRouter, useHistory } from 'react-router-dom';
-import React, { useState } from 'react'
+import React, {useEffect, useState } from 'react'
 import { useAuth } from '../context/authContext'
 import M from 'materialize-css';
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
-  });
+
 
 const Navbar = () => {
     const [error, setError] = useState("");
     const { gjeldeneBruker, loggut } = useAuth();
     const history = useHistory();
+
+    useEffect(() => { 
+      M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
+}, [])
+
 
     async function handleLoggut() {
         setError("");
@@ -22,27 +24,54 @@ const Navbar = () => {
         }catch {
             setError("Utlogging mislykkes")
         }
-    }
-    return ( 
-    <div className="App">
+    } if(!gjeldeneBruker){
+        return (<div className="App">
       <nav className="deep-purple darken-4">  
       <div className="nav-wrapper container nav">
-    
+        
       <a id="logo-container" href="/" className="brand-logo">USN-valget</a>
       <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <div className="">
               <ul className="right hide-on-med-and-down">
-                  {gjeldeneBruker && <li><NavLink to="/">Hjem</NavLink></li>}
-                  {gjeldeneBruker && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
-                  {gjeldeneBruker && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
-                  {gjeldeneBruker && <li><NavLink to="/BrukerProfil">Profil</NavLink></li>}
-                  {!gjeldeneBruker && <li><NavLink to="/Logginn">Logg inn</NavLink></li>}
-                  {!gjeldeneBruker && <li><NavLink to="/Registrering">Registrering</NavLink></li>}
-                  {gjeldeneBruker && <button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
+        {<li><NavLink to="/Logginn">Logg inn</NavLink></li>}
+        {<li><NavLink to="/Registrering">Registrering</NavLink></li>}
+        {error && <p>{error}</p>}
+        </ul>
+        <ul id="nav-mobile" className="sidenav">
+        {<li><NavLink to="/Logginn">Logg inn</NavLink></li>}
+        {<li><NavLink to="/Registrering">Registrering</NavLink></li>}
+        {<button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
+        </ul>
+        </div>
+        </div>
+        </nav>
+        </div> 
+        
+        )
+        
+
+    } else {
+    return ( 
+    <div className="App">
+      <nav className="deep-purple darken-4">  
+      <div className="nav-wrapper container nav">
+        
+      <a id="logo-container" href="/" className="brand-logo">USN-valget</a>
+      <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <div className="">
+              <ul className="right hide-on-med-and-down">
+                {gjeldeneBruker.emailVerified == false && <li><NavLink to="/Logginn">Logg inn</NavLink></li>}
+                {gjeldeneBruker.emailVerified == false && <li><NavLink to="/Registrering">Registrering</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/">Hjem</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/BrukerProfil">Profil</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
                   {error && <p>{error}</p>}
               </ul>
               
               <ul id="nav-mobile" className="sidenav">
+<<<<<<< HEAD
                 {gjeldeneBruker && <li><NavLink to="/">Hjem</NavLink></li>}
                   {gjeldeneBruker && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
                   {gjeldeneBruker && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
@@ -51,6 +80,17 @@ const Navbar = () => {
                   {!gjeldeneBruker && <li><NavLink to="/Registrering">Registrering</NavLink></li>}
                   {gjeldeneBruker && <button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
                   {error && <p>{error}</p>}   
+=======
+              {gjeldeneBruker.emailVerified == false && <li><NavLink to="/Logginn">Logg inn</NavLink></li>}
+                {gjeldeneBruker.emailVerified == false && <li><NavLink to="/Registrering">Registrering</NavLink></li>}   
+                {gjeldeneBruker.emailVerified == true && <li><NavLink to="/">Hjem</NavLink></li>}
+                {gjeldeneBruker.emailVerified == true && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <li><NavLink to="/BrukerProfil">Profil</NavLink></li>}
+                  {gjeldeneBruker.emailVerified == true && <button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
+                  {error && <p>{error}</p>}
+                  
+>>>>>>> 153a975aeca044acdceafd19201e7b7893515ed2
               </ul>
             </div>
             </div>
@@ -59,6 +99,7 @@ const Navbar = () => {
       
     
      );
+}
 }
  
 export default withRouter(Navbar);
