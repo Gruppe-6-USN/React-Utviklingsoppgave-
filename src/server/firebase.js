5fceb640cd10926c8ed4612e6b76639e240d327d
sebastian-gott <58074482+sebastian-gott@users.noreply.github.com>
2021-04-30 12:39:03 +0200

    dotenv variabel

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth"
 import "firebase/firestore"
 import 'firebase/storage'
 const app = firebase.initializeApp({
-      apiKey: "AIzaSyANAMlF4kBoupiP5OdqBfks21nHZFwHUM0",
-      authDomain: "app2000-dev.firebaseapp.com",
-      projectId: "app2000-dev",
-      storageBucket: "app2000-dev.appspot.com",
-      messagingSenderId: "577336654085",
-      appId: "577336654085:web:ce695160acf23a00ad9f6c"
+      apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+      appId: process.env.REACT_APP_FIREBASE_APP_ID
 });
 
 const storage = app.storage()
