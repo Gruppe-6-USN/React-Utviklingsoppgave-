c93751eb60c3fb9896fd91605fefd8755f3b1cc3
KristerLassen <70012970+KristerLassen@users.noreply.github.com>
2021-05-22 14:43:05 +0200

    skifta fra bilde til html på postern i home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useAuth } from '../context/authContext'
 import { storage } from "../server/firebase";
-import poster from './poster.jpg';
 
 
 export function Home(){
@@ -26,7 +25,7 @@ export function Home(){
 
 
        
-  
+
 
     return ( 
     <div className="App">
@@ -43,7 +42,17 @@ export function Home(){
                 <strong>Email: </strong> {gjeldeneBruker.email}
                 
                 
-            <img src= {poster} width="100%" height="auto" alt="poster"/>
+         
+                <div class = "background">
+                    <div class ="stor-text">VELKOMMEN TIL USN VALGET</div>
+                    <div class ="liten-text">GRUPPE 6 PRESENTERER</div>
+                    <div class="stor-text">EN NOMINERINGS APP</div>
+                    <div class ="liten-text"> I denne appen så skal dere velge fram en eller flere personer og stemme på hvilken person man vil ha som dette årets studentrådrepresentant</div>
+                    <div class="stor-text">For å nominere en person så må du:</div>
+                    <div class ="liten-text">Gå til nominering tabben i toppen av appen og velger den personen du eventuelt tenker at best kan passe som studentrådrepresentant. for å avkaste din stemme så går du til avstemming</div>
+                    <div class="stor-text">Du kan kun stemme på en person</div>
+                </div>
+            
 
             
                 
