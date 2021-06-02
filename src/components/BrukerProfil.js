459a73b14dc24060df7abfaff8087d78aaa28e23
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-06-02 10:47:30 +0200

    Update BrukerProfil.js

--- src/components/BrukerProfil.js
+++ src/components/BrukerProfil.js
@@ -135,7 +135,7 @@ export default function App() {
         <div className="row">
         
               <div className="col l2 push-s1 width-margin m5 card-panel myProfile">
-                {gjeldeneBruker.photoURL && <img src={ gjeldeneBruker.photoURL } width="100" height="100" alt="avatar" className="circle z-depth-2"/> }
+                {gjeldeneBruker.photoURL && <img src={ gjeldeneBruker.photoURL } width="100" height="100" alt="avatar" className="circle z-depth-2 profiBilde"/> }
                 <p className="profilStorTekst">{fornavnDisplay}  {etternavnDisplay}</p>
                 <p>Email: {gjeldeneBruker.email}</p>
               </div>
