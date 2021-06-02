d59a40bee6452b3833b993ccff8b0450a1c637b8
andersenmats <70013041+andersenmats@users.noreply.github.com>
2021-06-02 10:18:55 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -67,14 +67,14 @@ const Navbar = () => {
                   {gjeldeneBruker.emailVerified === true && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
                   {gjeldeneBruker.emailVerified === true && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
                   {gjeldeneBruker.emailVerified === true && <li><NavLink to="/BrukerProfil">Profil</NavLink></li>}
-                  { gjeldeneBruker.photoURL && <img src={ gjeldeneBruker.photoURL } onError={handleImageError} width="60" height="60" alt="avatar" className="circle z-depth-2"/> }
+                  { gjeldeneBruker.photoURL && <img src={ gjeldeneBruker.photoURL } onError={handleImageError} width="50" height="50" alt="avatar" className="circle z-depth-2 navBilde"/> }
                   {gjeldeneBruker.emailVerified === true && <button onClick={handleLoggut} className="loggutKnapp btn waves-effect waves-light right">Logg ut</button>}
                   {error && <p>{error}</p>}
               </ul>
               
               <ul id="nav-mobile" className="sidenav">
                 {<li><a className="sidenav-close material-icons right" href="#!">close</a></li>}
-                { gjeldeneBruker.photoURL && <img src={ gjeldeneBruker.photoURL } onError={handleImageError} width="100" height="100" alt="avatar" className="circle z-depth-2"/> }
+                { gjeldeneBruker.photoURL && <img src={ gjeldeneBruker.photoURL } onError={handleImageError} width="100" height="100" alt="avatar" className="circle z-depth-2 sideNavBilde"/> }
                 {gjeldeneBruker && <li><NavLink to="/">Hjem</NavLink></li>}
                   {gjeldeneBruker && <li><NavLink to="/Avstemming">Avstemming</NavLink></li>}
                   {gjeldeneBruker && <li><NavLink to="/Nominering">Nominering</NavLink></li>}
